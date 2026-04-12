80c0ffc02e916dd2b8282a83f84907c5f380e093
gersonmachado72 <gerson72m@gmail.com>
2026-04-12 18:34:39 -0300

    feat: Adiciona suporte a PostgreSQL, modelo Avaliacao e DataContext

---
+++ Models/Avaliacao.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoPizza.Models;
+
+public class Avaliacao
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public int PedidoId { get; set; }
+    
+    [Range(1, 5)]
+    public int Nota { get; set; }
+    
+    [StringLength(500)]
+    public string? Comentario { get; set; }
+    
+    public string? ClienteNome { get; set; }
+    public DateTime DataAvaliacao { get; set; } = DateTime.Now;
+    public bool Aprovado { get; set; } = false;
+}
--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -1,18 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoPizza.Models;
 
 public class Pedido
 {
     public int Id { get; set; }
+    
+    [Required]
     public string? NomeCliente { get; set; }
+    
+    [Required]
     public string? Endereco { get; set; }
+    
+    [Required]
+    [Phone]
     public string? Telefone { get; set; }
+    
     public DateTime DataPedido { get; set; }
-    public string? Status { get; set; }  // "Preparando", "Saiu para entrega", "Finalizado"
+    public string? Status { get; set; }
     public List<ItemPedido>? Itens { get; set; }
     public decimal ValorTotal { get; set; }
+    
+    public string? Observacao { get; set; }
+    public string? EntregadorNome { get; set; }
+    public DateTime? DataEntrega { get; set; }
+    public string? MetodoPagamento { get; set; }
+    public bool PagamentoConfirmado { get; set; }
+    public int RestaurantId { get; set; } = 1;
 }
 
 public class ItemPedido
@@ -23,4 +39,5 @@ public class ItemPedido
     public int Quantidade { get; set; }
     public decimal PrecoUnitario { get; set; }
     public decimal Subtotal => Quantidade * PrecoUnitario;
+    public string? ObservacaoItem { get; set; }
 }
