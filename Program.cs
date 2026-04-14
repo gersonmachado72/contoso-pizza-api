6004b33d8c7eb10e23b0af1c7d614d8b5bd65783
gersonmachado72 <gerson72m@gmail.com>
2026-04-14 11:43:09 -0300

    fix: Move inicialização do PedidoService para antes do app.Run()

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Data;
 using ContosoPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +9,36 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllersWithViews();
 
+// Configurar PostgreSQL
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+if (string.IsNullOrEmpty(databaseUrl))
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite("Data Source=contosopizza.db"));
+}
+else
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    var username = userInfo[0];
+    var password = userInfo[1];
+    var database = uri.AbsolutePath.TrimStart('/');
+    var connectionString = $"Host={uri.Host};Port={uri.Port};Database={database};Username={username};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+    
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
+
 var app = builder.Build();
+
+// ===== IMPORTANTE: Inicializar banco ANTES de rodar a aplicação =====
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+    PedidoService.Initialize(db);
+    Console.WriteLine("✅ Banco de dados inicializado e PedidoService configurado!");
+}
 
 if (app.Environment.IsDevelopment())
 {
